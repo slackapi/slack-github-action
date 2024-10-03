05b1c7cede440975d00ef14cdc574f8d8c3a1d02
@zimeg <zim@o526.net>
2024-10-03 02:13:36 -0700

    chore: replace hardcoding types with an upstream alternative

--- @types/markup-js.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "markup-js" {
-  /**
-   * @param template - The string with templated values.
-   * @param context - Values to replace in the template.
-   * @returns string - The template with replaced context values.
-   * @link https://github.com/adammark/Markup.js#usage
-   */
-  export function up(template: string, context: object): string;
-}
